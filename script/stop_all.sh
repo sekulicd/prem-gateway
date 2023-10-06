f62e4411d3a84a6503207e72330f6ca02630914b
sekulicd <sekula87@gmail.com>
2023-10-06 13:45:35 +0200

    fix script

--- script/stop_all.sh
+++ script/stop_all.sh
@@ -3,19 +3,23 @@
 set -e
 
 # Get all container IDs connected to the 'prem-gateway' network
-CONTAINERS=$(docker ps -aq --filter network=prem-gateway)
+CONTAINERS=($(docker ps -aq --filter network=prem-gateway))
 
 # Check if CONTAINERS is empty (no containers on the network)
-if [ -z "$CONTAINERS" ]; then
+if [ ${#CONTAINERS[@]} -eq 0 ]; then
     echo "No containers found on the 'prem-gateway' network."
 else
     # Stop all containers
     echo "Stopping containers on 'prem-gateway' network..."
-    docker stop "$CONTAINERS"
+    for CONTAINER in "${CONTAINERS[@]}"; do
+        docker stop "$CONTAINER"
+    done
 
     # Remove all containers and their anonymous volumes
     echo "Removing containers and cleaning up volumes..."
-    docker rm -v "$CONTAINERS"
+    for CONTAINER in "${CONTAINERS[@]}"; do
+        docker rm -v "$CONTAINER"
+    done
 
     echo "Containers stopped and removed. Volumes cleaned."
 fi
