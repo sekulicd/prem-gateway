dff52475a224ab71002f13227abd1fd993c81eba
sekulicd <sekula87@gmail.com>
2023-10-25 10:49:22 +0200

    fix

--- auth/internal/interface/http/handler/types.go
+++ auth/internal/interface/http/handler/types.go
@@ -3,9 +3,9 @@ package httphandler
 import "prem-gateway/auth/internal/core/application"
 
 type CreateApiKey struct {
-	Service          string
-	RequestsPerRange int
-	RangeInMinutes   int
+	Service          string `json:"service_name"`
+	RequestsPerRange int    `json:"requests_per_range"`
+	RangeInMinutes   int    `json:"range_in_minutes"`
 }
 
 func ToAppCreateApiKeyInfo(req CreateApiKey) application.CreateApiKeyReq {
